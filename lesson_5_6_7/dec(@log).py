a4638aa095c34f67e3be4cfdff296ed97cc64e2f
Dmitry-U-geekbrains <98094412+Dmitry-U-geekbrains@users.noreply.github.com>
2023-05-10 20:59:38 +0300

    task_7 (#7)

--- lesson_5_6_7/dec(@log).py
+++ lesson_5_6_7/dec(@log).py
@@ -4,8 +4,7 @@ import sys
 import logging
 import logs.config_server_log
 import logs.config_client_log
-import traceback
-import inspect
+
 
 if sys.argv[0].find('client') == -1:
     LOGGER = logging.getLogger('server')
@@ -17,12 +16,9 @@ def log(func_to_log):
     def log_saver(*args, **kwargs):
         ret = func_to_log(*args, **kwargs)
         LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
-                     f'Вызов из модуля {func_to_log.__module__}. Вызов из'
-                     f' функции {traceback.format_stack()[0].strip().split()[-1]}.'
-                     f'Вызов из функции {inspect.stack()[1][3]}')
+                     f'Вызов из модуля {func_to_log.__module__}')
         return ret
     return log_saver
-
 
 class Log:
     def __call__(self, func_to_log):
--- lesson_5_6_7/settings/ut.py
+++ lesson_5_6_7/settings/ut.py
@@ -1,8 +1,14 @@
 # утилиты
 
 import json
+import sys
 from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+from errors import IncorrectDataRecivedError, NonDictInputError
+from decos import log
+sys.path.append('../')
 
+
+@log
 def get_message(client):
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
@@ -10,11 +16,14 @@ def get_message(client):
         response = json.loads(json_response)
         if isinstance(response, dict):
             return response
-        raise ValueError
-    raise ValueError
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
+@log
 def send_message(sock, message):
+    if not isinstance(message, dict):
+        raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
