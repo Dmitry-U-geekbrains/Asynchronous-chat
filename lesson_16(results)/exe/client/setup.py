1485f920a577558c75160bd42fad892d9db40208
Dmitry-U-geekbrains <98094412+Dmitry-U-geekbrains@users.noreply.github.com>
2023-06-13 22:02:44 +0300

    task_final (#16)

---
+++ lesson_16(results)/exe/client/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='my_client_ushakov',
+      version='0.1',
+      description='my_client',
+      packages=find_packages(),  # ,Будем искать пакеты тут(включаем авто поиск пакетов)
+      author_email='ushakov.ip@yandex.ru',
+      author='Dmitry Ushakov',
+      install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
+      ##зависимости которые нужно до установить
+      )
